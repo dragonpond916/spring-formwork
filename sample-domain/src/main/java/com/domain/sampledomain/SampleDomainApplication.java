afcdb0c658a1be65b8cb1293cca99ed6d35723dd
dragonpond <dragonpond916@icloud.com>
2025-04-14 22:14:54 +0900

    초기 세팅 - 모듈생성

---
+++ sample-domain/src/main/java/com/domain/sampledomain/SampleDomainApplication.java
@@ -0,0 +1,13 @@
+package com.domain.sampledomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleDomainApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(SampleDomainApplication.class, args);
+  }
+  
+}
---
+++ sample-domain/src/test/java/com/domain/sampledomain/SampleDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domain.sampledomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class SampleDomainApplicationTests {
+  
+  @Test
+  void contextLoads() {
+  }
+  
+}
